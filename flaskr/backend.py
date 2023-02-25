6596b17d6a62cc391007614bdf163c141da1c1bc
Emmanuel Gonzalez Morales <emmanuel_gonzalez@techexchange.in>
2023-02-25 14:51:00 -0400

    [FEAT] user model and backend bucket and blob

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,8 +1,15 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+from google.cloud import storage
+from flask import Flask, request
+from user_model import User
+import hashlib
+
 class Backend:
 
     def __init__(self):
-        pass
+        self.storage_client = storage.Client()
+        self.bucket_content = storage.bucket("wikiviewer-content")
+        self.bucket_user_password = storage.bucket("user-passwords")
         
     def get_wiki_page(self, name):
         pass
@@ -13,12 +20,44 @@ class Backend:
     def upload(self):
         pass
 
+    @app.route("/signup/", method=["POST"])
     def sign_up(self):
-        pass
-
+        new_user_blob = self.bucket_user_password.blob("name?")
+        if request.method != "POST":
+            print("Invalid method request")
+        else:
+            username: str = "emmagonz22"#request.form["username"]
+            password: str = "123456789"#request.form["password"]
+            hash_password: str = "blake2b_"+hashlib.blake2b(password)
+            user = User(username, hash_password)
+            if not self.bucket_user_password.blob(username):
+                with new_user_blob.open("w") as new_user:
+                    new_user.write(user)
+            else:
+                print(f"User {username} already exist")
+            
+    @app.route("/signin/")
     def sign_in(self):
-        pass
+        if request.method != "POST":
+            print("Invalid method request")
+        else:
+            username = "emmagonz22"#request.form["username"]
+            password = "123456789"#request.form["password"]
+            hash_password: str = "blake2b_"+hashlib.blake2b(password)
+            user = User(username, hash_password)
+
+            if self.bucket_user_password.blob(username):
+                read_user_blob = self.bucket_user_password.blob(username)
+                with read_user_blob.open("r") as user_signin:
+                    print(user_signin.read())
+                    if user_signin.username == user.username and user_signin.hash_password == user.hash_password:
+                        print("Successful sign in")
+            else:
+                print(f"Wrong {username} or password")
+
 
     def get_image(self):
         pass
 
+bk = Backend()
+print(f"Bucket {bk.bucket_content.name} tested")
---
+++ flaskr/user_model.py
@@ -0,0 +1,22 @@
+from PIL import Image
+class User:
+
+    def __init__(self, username: str, hash_password: str, 
+                email: str,
+                first_name: str = "", second_name: str = "", 
+                description:str ="",
+                profile_picture: Image = None,
+                author_picture: Image = None):
+        self.username: str = username
+        self.email: str = email
+        #Using the hashlin blake2b with prefix blake2b_<hash>
+        self.hash_password: str = hash_password
+        self.first_name: str = first_name
+        self.second_name: str = second_name
+        self.description: str = description 
+        self.profile_picture = profile_picture
+        self.author_picture = author_picture
+
+
+    def __str__(self):
+        return f"User: {self.username}, Full name: {self.first_name} {self.second_name}, Description {self.description}"
