31e5607a7d5fee5de64109e1c203eff6ea523779
Emmanuel Gonzalez Morales <emmanuel_gonzalez@techexchange.in>
2023-03-05 02:10:50 +0000

    [REFACTOR] User model extended from UserMix and backend updated

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,9 +1,8 @@
 # TODO(Project 1): Implement Backend according to the requirements.
 from google.cloud import storage
-from user_model import User
-from page_model import Page
 import hashlib
-
+from user_model import User 
+from page_model import Page
 
 """
 
@@ -15,8 +14,10 @@ class Backend:
         self.storage_client = storage.Client()
         self.bucket_content = self.storage_client.bucket("wikiviewer-content")
         self.bucket_user_password = self.storage_client.bucket("user-passwords")
-    
+        self.active_user: User = None
+    """
 
+    """
     def get_wiki_page(self, name):
         content_blobs = self.storage_client.list_blobs(self.bucket_content.name)
         wiki_page = None
@@ -25,7 +26,10 @@ class Backend:
                 wiki_page = blob
                 break
         return wiki_page
+    """
 
+
+    """
     def get_all_page_names(self):#List of content in blob (pages) 
         content_blobs = self.storage_client.list_blobs(self.bucket_content.name)
         page_list = []
@@ -33,37 +37,37 @@ class Backend:
             page_list.append(blob.name)
         return page_list
 
-        
-    def upload(self): #Add content to the content-bucket (a blob object)
-        new_page_blob = self.bucket_content.blob("New page")
+    """
+
+
+    """
+    def upload(self, content_name, content): #Add content to the content-bucket (a blob object)
+        new_page_blob = self.bucket_content.blob(content_name)
         with new_page_blob.open("w") as f:
-            f.write("Upload")
+            f.write(content)
 
-    # For testing purpose 
-    def upload_page(self):
-       new_page_blob = self.bucket_content.blob("Test page")
-       new_page = Page("Test page", ["Content"], None)
-       with new_page_blob.open("w") as add_new_page:
-           add_new_page.write(new_page)
+    """
 
-    def sign_up(self):
-        new_user_blob = self.bucket_user_password.blob("name?")
-        username: str = 1
-        password: str = 1
-        hash_password: str = "blake2b_"+hashlib.blake2b(password)
-        user = User(username, hash_password)
+    """
+    def sign_up(self, username: str, password: str):
+        new_user_blob = self.bucket_user_password.blob(username)
+        salted_password = f"{username}_vacation2023_{password}"
+        hash_password: str = hashlib.blake2b(salted_password.encode()).hexdigest()
+        user: User = User(username, hash_password)
         if not self.bucket_user_password.blob(username):
             with new_user_blob.open("w") as new_user:
                 new_user.write(user)
         else:
             print(f"User {username} already exist")
-        
-    def sign_in(self):
+    """
+
+    """ 
+    def sign_in(self, username, password):
  
-        username = 1
-        password = 1
-        hash_password: str = "blake2b_"+hashlib.blake2b(password)
-        user = User(username, hash_password)
+        salted_password: str = f"{username}_vacation2023_{password}"
+        hash_password: str = hashlib.blake2b(salted_password.encode()).hexdigest()
+        user: User = User(username, hash_password)
+
 
         if self.bucket_user_password.blob(username):
             read_user_blob = self.bucket_user_password.blob(username)
@@ -71,9 +75,14 @@ class Backend:
                 print(user_signin.read())
                 if user_signin.username == user.username and user_signin.hash_password == user.hash_password:
                     print("Successful sign in")
+                    self.active_user = user_signin
         else:
             raise Exception(f"Wrong {username} or password")
+    """
 
-    def get_image(self):
-        pass
-
+    """
+    def get_image(self, image_name: str):
+        read_blob = self.bucket_content.blob(image_name)
+        with read_blob.open("r") as image:
+            return image.read()
+        
--- flaskr/user_model.py
+++ flaskr/user_model.py
@@ -1,12 +1,24 @@
+from flask_login import UserMixin
 
-class User:
+
+"""User model for the 
+
+User model extending the UserMixin class with feature to add more custom data from the User
+
+Typical usage example:
+
+  user = User(username, password)
+  isActiveUser = user.is_active() 
+"""
+
+
+class User(UserMixin):
 
     def __init__(self, username: str, hash_password: str, 
                 email: str,
                 first_name: str = "", second_name: str = "", 
                 description:str ="",
-                profile_picture: str = None,
-                author_picture: str = None):
+                profile_picture: str = None):
         self.username: str = username
         self.email: str = email
         #Using the hashlin blake2b with prefix blake2b_<hash>
@@ -15,8 +27,9 @@ class User:
         self.second_name: str = second_name
         self.description: str = description 
         self.profile_picture = profile_picture
-        self.author_picture = author_picture
 
-
+    """
+    Convert the User object to string with user basic information
+    """
     def __str__(self):
         return f"User: {self.username}, Full name: {self.first_name} {self.second_name}, Description {self.description}"
