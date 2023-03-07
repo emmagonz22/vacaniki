95b1ce884b72089d653447bcf9d6fabb5d33c69e
Cesar Olague <czar_olag@techexchange.in>
2023-03-07 01:17:05 +0000

    [INIT] Added Initializing to LoginManager

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,6 +12,25 @@ def create_app(test_config=None):
     # Create and configure the app.
     app = Flask(__name__, instance_relative_config=True)
 
+
+    #Reference = "https://flask-login.readthedocs.io/en/latest/#how-it-works%20for%20more%20info"
+    # Create a LoginManager instance
+    login_manager = LoginManager()
+
+    # Configure LoginManager
+    login_manager.init_app(app)
+
+    # Call methods on the login_manager instance as needed
+    login_manager.login_view = 'login'
+
+    # Define user_loader callback
+    @login_manager.user_loader
+    def load_user(username):
+    # Retrieve user
+        user_id = hashlib.blake2b(username.encode('utf-8')).hexdigest()
+        return User.get(user_id)
+
+
     # This is the default secret key used for login sessions
     # By default the dev environment uses the key 'dev'
     app.config.from_mapping(
