909a16b35e818528fbf04f88907f7952ea2c3346
Cesar Olague <czar_olag@techexchange.in>
2023-03-07 01:13:59 +0000

    [FIX] Fixed login route.

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, render_template, request, url_for
-from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from flask import Flask, redirect, render_template, request, url_for, flash
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
 from google.cloud import storage
 from flaskr.backend import Backend
+from flaskr.user_model import User
 import hashlib
 
 def make_endpoints(app):
@@ -13,7 +14,7 @@ def make_endpoints(app):
     def home():
         return render_template('main.html')
 
-
+    
     @app.route('/signup/', methods=['GET', 'POST'])
     def sign_up():
         if request.method == 'GET':
@@ -25,14 +26,19 @@ def make_endpoints(app):
             # sends user and pass to backend to be verified/sent to bucket and signed in
             sign_up_event = backend.sign_up(username=username, password=password)
             if sign_up_event:
-                print('Signed up successfully!')
+                user = User(username=username)
+                login_user(user)
+                print(User, 'USER INFO')
+                flash('Signed up successfully!')
+                flash(str(user))
+                return redirect(url_for('sign_up'))
             else:
-                print('Account already exist!')
-            return render_template('main.html')
+                flash('Account already exist!')
+                return redirect(url_for('sign_up'))
         else:
-            return 'Invalid method request'
+            return redirect(url_for('sign_up'))
 
-
+   
     @app.route('/login/', methods=['GET', 'POST'])
     def login():
         if request.method == "GET":
@@ -43,35 +49,41 @@ def make_endpoints(app):
             password: str = request.form['password']
 
             # sends user and pass to backend to be verified and logged in
-            backend.sign_in(username=username, password=password)
-            print('Logged in successfully!')
-            return render_template('main.html')
+            sign_up_event = backend.sign_in(username=username, password=password)
+            if sign_up_event:
+                user = User(username=username)
+                login_user(user)
+                flash('Logged in successfully!')
+                return redirect(url_for('home'))
+            else:
+                flash('Wrong username or password. Please Try Again.')
+                return redirect(url_for('login'))
         else: 
-            return 'Invalid method request'
+            return redirect(url_for('login'))
 
-
+    @login_required
     @app.route('/logout/', methods=['GET'])
     def logout():
         # using flask-login module, logouts user and redirects to home page
         logout_user()
         return redirect(url_for('home'))
 
-
+    @login_required
     @app.route('/upload/', methods=['GET','POST'])
     def upload():
         if request.method == 'POST':
             if 'file' not in request.files:
-                print('No File Input')
+                flash('No File Input')
                 return redirect(url_for('upload'))
 
             #grab wikipage name and file content
             wikipage: str = request.form['wikipage']
             file = request.files['file']
             if file.filename == '':
-                print('No selected file')
+                flash('No selected file')
                 return redirect(url_for('upload'))
             if file:
                 backend.upload(wikipage, file)
-                print('File uploaded successfully')
-                return render_template('main.html')
-        return render_template('upload.html')
+                flash('File uploaded successfully')
+                return redirect(url_for('home'))
+        return redirect(url_for('upload'))
