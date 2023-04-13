5a0dbccd9ddf9e04d875bd66976bc4d97667f7b9
Emmanuel Gonzalez Morales <emmanuel_gonzalez@techexchange.in>
2023-04-13 06:11:42 +0000

    [FEATURE] Cesar requirement #1 remove content user to Deleted_USer prefix

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -2,7 +2,7 @@ from flaskr import create_app
 from io import BytesIO
 
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, Mock, MagicMock, patch, create_autospec, mock_open
 
 
 # See https://flask.palletsprojects.com/en/2.2.x/testing/
@@ -61,8 +61,21 @@ def test_get_image(mock_get_image, client):
 
 
 def test_signup_page(client):
-    with patch('flaskr.backend.Backend.sign_up') as mock_sign_up:
+    with patch('flaskr.backend.Backend.sign_up') as mock_sign_up, patch(
+            'flaskr.user_model.User') as mock_user:
         # Successful signup
+        mock_sign_up.bucket_user_password.blob.return_value = MagicMock()
+        mock_sign_up.user_data_bucket.blob.return_value = MagicMock()
+
+        # Mock Backend object and its methods
+
+        mock_user.return_value.get_user_data.return_value = {
+            'username': 'cooldude2006',
+            'email': 'cooldude2006@example.com',
+            'name': 'Cool Dude',
+            'description': 'Just a cool dude',
+        }
+
         mock_sign_up.return_value = True
         data = {'username': 'Cesar', 'password': '123'}
         resp = client.post("/signup/", data=data, follow_redirects=True)
@@ -78,8 +91,21 @@ def test_signup_page(client):
 
 
 def test_login_page(client):
-    with patch('flaskr.backend.Backend.sign_in') as mock_login:
-        # Successful Login
+    with patch('flaskr.backend.Backend.sign_in') as mock_login, patch(
+            'flaskr.user_model.User') as mock_user:
+
+        # Successful Login]
+
+        mock_login.bucket_user_password.blob.return_value = MagicMock()
+        mock_login.user_data_bucket.blob.return_value = MagicMock()
+
+        mock_user.return_value.get_user_data.return_value = {
+            'username': 'cooldude2006',
+            'email': 'cooldude2006@example.com',
+            'name': 'Cool Dude',
+            'description': 'Just a cool dude',
+        }
+
         mock_login.return_value = True
         data = {'username': 'cooldude2006', 'password': '12345'}
         resp = client.post("/login/", data=data, follow_redirects=True)
