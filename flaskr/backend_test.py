f21e2159fdb23d1a42256847228bdbfb91e90276
Emmanuel Gonzalez Morales <emmanuel_gonzalez@techexchange.in>
2023-04-25 04:56:42 +0000

    Format

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -132,37 +132,39 @@ def test_upload():
     mock_blob = MagicMock()
     mock_blob.return_value = b'test data'
     backend.bucket_content.blob.return_value = mock_blob
-    
+
     mock_user_data = {
-            'username': username,
-            'name': '',
-            'email': '',
-            'uploaded_wiki': [],
-            'uploaded_image': [],
-            'created_at': '',
-            'description': '',
-            'profile_photo': False
-        }
+        'username': username,
+        'name': '',
+        'email': '',
+        'uploaded_wiki': [],
+        'uploaded_image': [],
+        'created_at': '',
+        'description': '',
+        'profile_photo': False
+    }
     mock_json = MagicMock()
     mock_json.loads.return_value = mock_user_data
-    
-    with patch('json.dumps', mock_json.dumps), patch('json.loads', mock_json.loads), patch.object(Backend, 'upload_file_registry') as mock_upload_file_registry:
+
+    with patch('json.dumps', mock_json.dumps), patch(
+            'json.loads', mock_json.loads), patch.object(
+                Backend, 'upload_file_registry') as mock_upload_file_registry:
         backend.upload(username, content_name, content)
-        
-        backend.bucket_content.blob.assert_called_with(f'{username}/{content_name}')
+
+        backend.bucket_content.blob.assert_called_with(
+            f'{username}/{content_name}')
         mock_blob.upload_from_file.assert_called_with(
             content, content_type=content.content_type)
         mock_upload_file_registry.assert_called_with(username)
-        
+
         expected_json = json.dumps(mock_user_data)
         mock_json.dumps.assert_called_once_with(mock_user_data)
     #mock_data_blob = MagicMock()
     #mock_data_blob.download_as_string.return_value = json.dumps(mock_user_data)
     #backend.user_data_bucket.get_blob.return_value = mock_data_blob
 
-
     #call upload method
-  
+
 
 def test_get_image_failure():
     # Create a mock GCS bucket and blob
